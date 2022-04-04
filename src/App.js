5a1488c3b9809db6fb968566333e4f16790379e8
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-04-04 20:48:16 +0530

    App commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import CreateContainer from "./components/CreateContainer";
 
 function App() {
   return (
-    <AnimatePresence>
+    <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex flex-col bg-primary">
         <Header />
-        <main className='mt-24 p-8 w-full'>
+        <div className='mt-24 md:mt-24 p-8 w-full'>
           <Routes>
             <Route path="/*" element={<MainContainer />} />
             <Route path="/createItem" element={<CreateContainer />} />
           </Routes>
-        </main>
+        </div>
       </div>
     </AnimatePresence>
   );
