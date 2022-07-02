103e70798865cab8d5e9ec1b2eed09e23c5d885f
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-07-02 23:42:08 +0530

    Added lots of thing

--- Advance_DOM/script.js
+++ Advance_DOM/script.js
@@ -63,3 +63,200 @@ document.querySelector(".btn--close-cookie").addEventListener("click", () => {
   message.remove();
   //   message.parentElement.removeChild(message)
 });
+
+const btnScroll = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+
+btnScroll.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // -------  Scrolling -----
+
+  section1.scrollIntoView({ behavior: "smooth" });
+
+  //   const s1Scrolls = section1.getBoundingClientRect();
+  //   window.scrollTo({
+  //     left: s1Scrolls.left + window.pageXOffset,
+  //     top: s1Scrolls.top + window.pageYOffset,
+  //     behavior: "smooth",
+  //   });
+});
+// -------Event Delegation------
+
+// 1. Add event listener to common parent elements
+// 2. Determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+});
+
+// ------Tabs Components ------
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", (e) => {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Guard clause
+  if (!clicked) return;
+
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+
+  clicked.classList.add("operations__tab--active");
+
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
+// ------MENU HOVER ------
+
+const nav = document.querySelector("nav");
+
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+    siblings.forEach((el) => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener("mouseover", (e) => {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener("mouseout", (e) => {
+  handleHover(e, 1);
+});
+
+// -----Sticky Navbar------
+
+const initialCoard = section1.getBoundingClientRect();
+window.addEventListener("scroll", function () {
+  if (window.screenY > initialCoard.top) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+});
+// console.log("LINK", e.target, e.currentTarget);
+// console.log(e.currentTarget === this);
+
+// tabs.forEach((el) => {
+//   el.addEventListener("click", (e) => {
+//     console.log("Click");
+//   });
+// });
+
+// -----DOM Traversing--------
+// const h1 = document.querySelector("h1");
+
+// console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+
+// h1.firstElementChild.style.color = "white";
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+// h1.closest("h1").style.background = "var(--gradient-primary)";
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextSibling);
+// console.log(h1.previousSibling);
+
+// ----Page Navigation-----
+// document.querySelectorAll(".nav__link").forEach((el) => {
+//   el.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     const id = e.target.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
+
+// -----Event Propagation ------
+
+// const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+
+// document.querySelector(".nav__link").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log("LINK", e.target, e.currentTarget);
+//   console.log(e.currentTarget === this);
+
+// Stop Propagation
+// e.stopPropagation();
+// });
+// document.querySelector(".nav__links").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector(".nav").addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// -----EVENTS-----
+
+// const h1 = document.querySelector("h1");
+
+// const alertH1 = (e) => {
+//   alert("Window");
+// };
+// h1.addEventListener("mouseenter", alertH1);
+
+// setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
+
+// h1.onmouseenter(() => {
+//   alert("welcome");
+// });
+
+// ------Style, Attributes and classes
+
+// message.style.backgroundColor = "#37383d";
+// message.style.width = "120%";
+
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 40 + "px";
+
+// //   To change the root variable in CSS
+// // document.documentElement.style.setProperty("--color-primary", "orange");
+
+// // ---Attributes--
+
+// const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.src);
+// logo.alt = "Logo only";
+// logo.getAttribute("src");
+
+// console.log(logo.getAttribute("designer"));
+
+// const link = document.querySelector(".twitter-link");
+// console.log(link.href);
+
+// console.log(logo.dataset.versionNumber);
+
+// // -----CLASSES-----
+// logo.classList.add("class1", "class2");
+// logo.classList.remove("class1", "class2");
+// logo.classList.toggle("class1", "class2");
+// logo.classList.contains("class1", "class2");
