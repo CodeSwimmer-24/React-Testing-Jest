2894a58f72a08c5ff1054ff020863c4c70585114
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-07-12 16:32:03 +0530

    MAPTY

--- MAPTY/script.js
+++ MAPTY/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
@@ -22,6 +21,14 @@ class Workout {
     this.duration = duration;
     this.distance = distance;
   }
+  // _setDescription() {
+  //   // prettier-ignore
+  //   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  //   this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+  //     months[this.date.getMonth()]
+  //   } ${this.date.getDate()}
+  //   }`;
+  // }
 }
 
 class Running extends Workout {
@@ -29,6 +36,7 @@ class Running extends Workout {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPace();
+    // this._setDescription();
   }
   calcPace() {
     this.pace = this.duration / this.distance;
@@ -41,6 +49,7 @@ class Cycling extends Workout {
     super(coords, distance, duration);
     this.cadence = elevationGain;
     this.calSpeed();
+    // this._setDescription();
   }
   calSpeed() {
     this.speed = this.distance / (this.duration / 60);
@@ -143,17 +152,21 @@ class App {
       workout = new Cycling([lat, lng], distance, duration, elevation);
       // Add new object to workout array
       this.#workout.push(workout);
+
       console.log(workout);
+
+      _renderWorkout(workout);
+      inputCadence.value =
+        inputDistance.value =
+        inputElevation.value =
+        inputDuration.value =
+          "";
     }
 
     // Render workout on map as marker
     // Render workout on list
     // Hide form + clear data
-    inputCadence.value =
-      inputDistance.value =
-      inputElevation.value =
-      inputDuration.value =
-        "";
+
     // const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -168,6 +181,53 @@ class App {
       )
       .setPopupContent("Workout")
       .openPopup();
+  }
+  _renderWorkout(workout) {
+    let html = `li class="workout workout--${workout.type}" data-id="${
+      workout.id
+    }">
+    <h2 class="workout__title">${workout.description}</h2>
+    <div class="workout__details">
+      <span class="workout__icon">${
+        workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"
+      }</span>
+      <span class="workout__value">${workout.distance}</span>
+      <span class="workout__unit">km</span>
+    </div>
+    <div class="workout__details">
+      <span class="workout__icon">⏱</span>
+      <span class="workout__value">${workout.duration}</span>
+      <span class="workout__unit">min</span>
+    </div>`;
+
+    if (workout.type === "running")
+      html += `
+    <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+    `;
+
+    if (workout.type === "cycling")
+      html += `
+    <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+    `;
+    form.insertAdjacentHTML("afterend", html);
   }
 }
 
