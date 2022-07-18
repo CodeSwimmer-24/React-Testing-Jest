35c34cd82a64148270cea514fa21a04850520640
Fahad Mahmood <79140036+CodeSwimmer-24@users.noreply.github.com>
2022-07-18 11:53:10 +0530

    recipe.js

--- Recipe/src/js/model.js
+++ Recipe/src/js/model.js
@@ -1,30 +1,52 @@
 import { async } from "regenerator-runtime";
-
+import { API_URL } from "./config";
+import { getJson } from "./helpers";
 export const state = {
   recipe: {},
+  search: {
+    query: "",
+    result: [],
+  },
 };
 
 export const loadRecipe = async (id) => {
-  const response = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-  );
-  const data = await response.json();
+  try {
+    const data = await getJson(`${API_URL}/${id}`);
 
-  if (!response.ok) {
-    throw new Error(`Something is wrong ${data.message} (${response.status})`);
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      image: recipe.image_url,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+  } catch (err) {
+    throw err;
   }
-  console.log(data);
+};
 
-  const { recipe } = data.data;
+export const loadSearchResult = async (query) => {
+  try {
+    state.search.query = query;
+    const data = await getJson(`${API_URL}/?search=${query}`);
+    // console.log(data);
 
-  state.recipe = {
-    id: recipe.id,
-    title: recipe.title,
-    image: recipe.image_url,
-    publisher: recipe.publisher,
-    sourceUrl: recipe.source_url,
-    servings: recipe.servings,
-    cookingTime: recipe.cooking_time,
-    ingredients: recipe.ingredients,
-  };
+    state.search.result = data.data.recipes.map((rec) => {
+      return {
+        id: rec.id,
+        title: rec.title,
+        image: rec.image_url,
+        publisher: rec.publisher,
+        sourceUrl: rec.source_url,
+      };
+    });
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
